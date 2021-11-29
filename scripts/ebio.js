6f0fdf9767ef01a4246ed635fca15ffac1857cc1
Nirash10qbit <nirash@10qbit.com>
2021-11-29 18:55:00 +0530

    bio

---
+++ scripts/ebio.js
@@ -0,0 +1,100 @@
+
+$(document).ready(function () {
+    twoByTwo();
+});
+ 
+
+function twoByTwo() 
+{
+   var genarateTag = "";
+    genarateTag += "<div class='card card-body bg-info' >";
+    genarateTag += "<table >";
+    for (let tr = 0; tr < 3; tr++) {
+        genarateTag += "  <tr id='tbtTR"+tr+"'  >";
+        for (let td = 0; td < 3; td++) {
+            genarateTag += "      <td id='tbtTR"+tr+"TD"+td+"'  class='"+(tr != 0 && td != 0 ?'border':'')+"'  style='"+(tr != 0 && td != 0 ?'width:100px; height:100px;':'width:20px; height:5px;')+"' >";
+            if(tr==0 && td==0){
+                continue;
+            }
+                if(tr==0 || td==0)
+                {
+                    genarateTag += "<div class='row'>";
+                    genarateTag += "<input type='text' id='TR"+tr+"TD"+td+"' onchange='changeSize(this)' class='form-control form-control-sm' style='"+(tr != 0 && td != 0 ?'':'width:30px; height:10px;')+"' value='"+(parseFloat(tr) +parseFloat(td) +1) +"' />";
+                   if((tr== 0 && td==1)||tr== 1 && td==0)
+                    genarateTag += "<input type='text' id='LTR"+tr+"TD"+td+"' onchange='changeSize(this)' class='form-control form-control-sm' style='"+(tr != 0 && td != 0 ?'':'width:30px; height:10px;')+"' value='x' />";
+                    genarateTag += "</div>";
+                } else{
+                    genarateTag += "<label id='TR"+tr+"TD"+td+"'  >TR"+tr+"TD"+td+"</label>";
+                }
+            genarateTag += "      </td>";
+        }
+        genarateTag += "  </tr>";
+    }
+    genarateTag += "</table>";
+    genarateTag += "</div>";
+    $('#divGen').text("");
+    $('#divGen').append(genarateTag);
+    formula();
+  }
+
+  function changeSize(controle) {
+      debugger
+      if(controle.id == "TR1TD0" || controle.id == "TR2TD0"){
+          var tr1=parseFloat($('#TR1TD0').val())+parseFloat($('#TR2TD0').val());
+          var td1Pr = (parseFloat($('#TR1TD0').val()) / tr1)*100;
+          var td2Pr = (parseFloat($('#TR2TD0').val()) / tr1)*100;
+      $('#tbtTR1TD1').css('height',(2*td1Pr) +'px');
+      $('#tbtTR1TD2').css('height',(2*td1Pr) +'px');
+
+      $('#tbtTR2TD1').css('height',(2*td2Pr) +'px');
+      $('#tbtTR2TD2').css('height',(2*td2Pr) +'px');
+
+    
+    }
+    if(controle.id == "TR0TD1" || controle.id == "TR0TD2"){
+        var td1=parseFloat($('#TR0TD1').val())+parseFloat($('#TR0TD2').val());
+        var tr1Pr = (parseFloat($('#TR0TD1').val()) / td1)*100;
+        var tr2Pr = (parseFloat($('#TR0TD2').val()) / td1)*100;
+
+        $('#tbtTR1TD1').css('width',(2*tr1Pr) +'px');
+        $('#tbtTR1TD2').css('width',(2*tr2Pr) +'px');
+
+        $('#tbtTR2TD1').css('width',(2*tr1Pr) +'px');
+        $('#tbtTR2TD2').css('width',(2*tr2Pr) +'px');
+      }
+
+      formula();
+
+  }
+  function formula() {
+       //formula call
+var val1 = parseFloat($('#TR0TD1').val())
+var val2 = parseFloat($('#TR1TD0').val())
+var val3 = parseFloat($('#TR0TD2').val())
+var val4 = parseFloat($('#TR2TD0').val())
+$('#TR1TD1').text('('+($('#TR0TD1').val()+'' + $('#LTR1TD0').val())+')('+$('#TR1TD0').val()+'' + $('#LTR0TD1').val()+')');
+$('#TR1TD2').text('('+($('#TR0TD1').val()+'' + $('#LTR1TD0').val())+')('+$('#TR0TD2').val()+')');
+$('#TR2TD1').text('('+($('#TR2TD0').val()+')('+$('#TR0TD1').val()+'' + $('#LTR0TD1').val()+')'));
+$('#TR2TD2').text('('+$('#TR0TD2').val()+')'+'('+$('#TR2TD0').val()+')');
+
+$('#Ans1').text('('+($('#TR1TD0').val()+'' + $('#LTR1TD0').val())+' + '+$('#TR2TD0').val()+') ('+($('#TR0TD1').val()+'' + $('#LTR1TD0').val())+' + '+$('#TR0TD2').val()+')');
+$('#Ans2').text('('+($('#TR0TD1').val()+'' + $('#LTR1TD0').val())+')('+($('#TR1TD0').val()+'' + $('#LTR1TD0').val())+') + ('+($('#TR0TD1').val()+'' + $('#LTR1TD0').val())+')('+$('#TR2TD0').val()+') + ('+($('#TR1TD0').val()+'' + $('#LTR1TD0').val())+')('+$('#TR0TD2').val()+') + ('+($('#TR2TD0').val())+')('+$('#TR0TD2').val()+')');
+$('#Ans3').text('');
+$('#Ans3').append((parseFloat($('#TR1TD0').val())*parseFloat($('#TR0TD1').val())))
+.append($('#LTR1TD0').val()+'<sup>2</sup>' )
+.append(' + ')
+.append((parseFloat($('#TR0TD1').val())*parseFloat($('#TR2TD0').val()))).append($('#LTR1TD0').val())
+.append(' + ')
+.append((parseFloat($('#TR1TD0').val())*parseFloat($('#TR0TD2').val()))).append($('#LTR1TD0').val())
+.append(' + ')
+.append(parseFloat($('#TR2TD0').val())*parseFloat($('#TR0TD2').val()));
+
+$('#Ans4').text('');
+$('#Ans4').append((parseFloat($('#TR1TD0').val())*parseFloat($('#TR0TD1').val())))
+.append($('#LTR1TD0').val()+'<sup>2</sup>' )
+.append(' + ')
+.append((parseFloat($('#TR0TD1').val())*parseFloat($('#TR2TD0').val()))+(parseFloat($('#TR1TD0').val())*parseFloat($('#TR0TD2').val()))).append($('#LTR1TD0').val())
+.append(' + ')
+.append(parseFloat($('#TR2TD0').val())*parseFloat($('#TR0TD2').val()));
+
+  }
---
+++ scripts/setsat5.js
@@ -0,0 +1,310 @@
+var selectedSection = 'A';
+var audio = new Audio("../audio/tsets.m4a");
+var number =100;
+
+$(document).ready(function () {
+    $('#imgImage2').css('visibility', 'hidden');
+   VisibleTrueButtons();
+   
+ 
+  $("#imgImage").attr("src","../Images/tn.png");
+
+    $('#dropDownType').change(function (e) {
+        check($('#dropDownType').val());
+    });
+
+    $('#btnSetsReset').click(function (e) {
+        location.reload();
+    });
+    
+    $('#imgImage2').click(function () {
+        $("#imgImage2").attr("src","../Images/next1.png");
+        $("#imgImage").attr("src","../Images/aabb.png");
+        var abc = $("#imgImage").attr("src");
+        $('.example-table').css('background-image', 'url(../Images/SetB_N.png)');
+       
+        $('#dropDownType').val("B_N");
+        VisibleFalseButtons();
+
+        if(number==0){
+            $("#imgImage").attr("src","../Images/t1.png");
+            $('#dragtarget1').css('visibility', 'visible');
+            $('.example-table').css('background-image', 'url(../Images/SetAUB_N.png)');
+            new Audio("../audio/t1.m4a").play();
+
+            number=1;
+        }
+        else if(number == 1){
+            $("#imgImage").attr("src","../Images/t2.png");
+            $('#dragtarget1').css('visibility', 'visible');
+            $('#dragtarget2').css('visibility', 'visible');
+            $('.example-table').css('background-image', 'url(../Images/SetAA.png)');
+            new Audio("../audio/t2.m4a").play();
+            number=4;
+        }
+        else if(number == 4){
+            $("#imgImage").attr("src","../Images/t4.png");
+            $('#dragtarget1').css('visibility', 'visible');
+            $('#dragtarget2').css('visibility', 'visible');
+            $('#dragtarget4').css('visibility', 'visible');
+            $('.example-table').css('background-image', 'url(../Images/SetA^B.png)');
+            new Audio("../audio/t4.m4a").play();
+            number=6;
+        }
+        else if(number == 6){
+            $("#imgImage").attr("src","../Images/t6.png");
+            $('#dragtarget1').css('visibility', 'visible');
+            $('#dragtarget2').css('visibility', 'visible');
+            $('#dragtarget4').css('visibility', 'visible');
+            $('#dragtarget6').css('visibility', 'visible');
+            $('.example-table').css('background-image', 'url(../Images/SetBB.png)');
+            new Audio("../audio/t6.m4a").play();
+            number=8;
+        }
+        else if(number == 8){
+            $("#imgImage").attr("src","../Images/ab5.png");
+            $('#dragtarget1').css('visibility', 'visible');
+            $('#dragtarget2').css('visibility', 'visible');
+            $('#dragtarget4').css('visibility', 'visible');
+            $('#dragtarget6').css('visibility', 'visible');
+            $('.example-table').css('background-image', 'url(../Images/SetB_N.png)');
+            $("#imgImage2").css("visibility","hidden");
+            new Audio("../audio/tze.m4a").play();
+        }
+       
+        else{
+          //  $("#imgImage").attr("src","../Images/aa1.png");
+        }
+        if(number == 100){
+        new Audio("../audio/te.m4a").play();
+        number = 0;
+        }
+    });
+});
+
+function selectSet() {
+    selectedSection = 'B';
+}
+
+function dragStart(event) {
+    event.dataTransfer.setData("Text", event.target.id);
+
+}
+
+function dragging(event) {
+    //document.getElementById("demo").innerHTML = "The p element is being dragged";
+}
+
+function allowDrop(event) {
+    event.preventDefault();
+}
+
+function drop(event) {
+    event.preventDefault();
+    var data = event.dataTransfer.getData("Text");
+    event.target.appendChild(document.getElementById(data));
+    check($('#dropDownType').val());
+}
+
+function check(action) {
+    var text = "";
+    if (action == 'A') {
+        $('.example-table').css('background-image', 'url(../Images/SetA.png)');
+        var list = document.getElementById('divSetA').children;
+        for (var i = 0; i < list.length; i++) {
+            var el = list[i].lastChild;
+            text += el.textContent + " , ";
+        }
+        var list = document.getElementById('divSetA_B').children;
+        for (var i = 0; i < list.length; i++) {
+            var el = list[i].lastChild;
+            text += el.textContent + " , ";
+        }
+        if (text != '')
+            text = text.substring(0, text.length - 2);
+        $('#btnA').text("A = { " + text + " }");
+    } else if (action == 'B') {
+        //B
+        $('.example-table').css('background-image', 'url(../Images/SetB.png)');
+        text = "";
+        var list = document.getElementById('divSetB').children;
+        for (var i = 0; i < list.length; i++) {
+            var el = list[i].lastChild;
+            text += el.textContent + " , ";
+        }
+        var list = document.getElementById('divSetA_B').children;
+        for (var i = 0; i < list.length; i++) {
+            var el = list[i].lastChild;
+            text += el.textContent + " , ";
+        }
+        if (text != '')
+            text = text.substring(0, text.length - 2);
+        $('#btnA').text("B = { " + text + " }");
+    } else if (action == 'AUB') {
+        //AUB
+        $('.example-table').css('background-image', 'url(../Images/SetAUB.png)');
+        text = "";
+        var list = document.getElementById('divSetA').children;
+        for (var i = 0; i < list.length; i++) {
+            var el = list[i].lastChild;
+            text += el.textContent + " , ";
+        }
+        var list = document.getElementById('divSetB').children;
+        for (var i = 0; i < list.length; i++) {
+            var el = list[i].lastChild;
+            text += el.textContent + " , ";
+        }
+        var list = document.getElementById('divSetA_B').children;
+        for (var i = 0; i < list.length; i++) {
+            var el = list[i].lastChild;
+            text += el.textContent + " , ";
+        }
+        if (text != '')
+            text = text.substring(0, text.length - 2);
+        $('#btnA').text("AUB = { " + text + " }");
+
+    } else if (action == 'A^B') {
+        $('.example-table').css('background-image', 'url(../Images/SetA^B.png)'); text = "";
+        text = "";
+        var list = document.getElementById('divSetA_B').children;
+        for (var i = 0; i < list.length; i++) {
+            var el = list[i].lastChild;
+            text += el.textContent + " , ";
+        }
+        if (text != '')
+            text = text.substring(0, text.length - 2);
+        $('#btnA').text("A^B = { " + text + " }");
+
+    } else if (action == 'A_N') {
+        //A'
+        $('.example-table').css('background-image', "url(../Images/SetA_N.png)");
+        text = "";
+        var list = document.getElementById('tblMain').children;
+        for (let tr = 0; tr < 3; tr++) {
+            for (let td = 0; td < 5; td++) {
+                for (var i = 0; i < list[0].children[tr].children[td].children.length; i++) {
+                    if ((tr == 1 && td == 1) || tr == 1 && td == 2 || tr == 1 && td == 3)
+                        continue
+                    var el = list[0].children[tr].children[td].children[i].lastChild;
+                    if (el != null)
+                        text += el.textContent + " , ";
+                }
+            }
+        }
+        for (var i = 0; i < document.getElementById('divSetB').children.length; i++) {
+            text += document.getElementById('divSetB').children[i].textContent + " , ";
+        }
+        if (text != '')
+            text = text.substring(0, text.length - 2);
+        $('#btnA').text("A' = { " + text + " }");
+    } else if (action == 'B_N') {
+        //B'
+        $('.example-table').css('background-image', "url(../Images/SetB_N.png)");
+        text = "";
+        var list = document.getElementById('tblMain').children;
+        for (let tr = 0; tr < 3; tr++) {
+            for (let td = 0; td < 5; td++) {
+                for (var i = 0; i < list[0].children[tr].children[td].children.length; i++) {
+
+                    if ((tr == 1 && td == 1) || tr == 1 && td == 2 || tr == 1 && td == 3)
+                        continue
+
+                    var el = list[0].children[tr].children[td].children[0].lastChild;
+                    if (el != null)
+                        text += el.textContent + " , ";
+                }
+            }
+        }
+        for (var i = 0; i < document.getElementById('divSetA').children.length; i++) {
+            text += document.getElementById('divSetA').children[i].textContent + " , ";
+        }
+
+        if (text != '')
+            text = text.substring(0, text.length - 2);
+        $('#btnA').text("B' = { " + text + " }");
+
+    } else if (action == 'AUB_N') {
+        //(AUB)'
+        $('.example-table').css('background-image', "url(../Images/SetAUB_N.png)");
+        text = "";
+        var list = document.getElementById('tblMain').children;
+        for (let tr = 0; tr < 3; tr++) {
+            for (let td = 0; td < 5; td++) {
+                for (var i = 0; i < list[0].children[tr].children[td].children.length; i++) {
+                    if ((tr == 1 && td == 1) || (tr == 1 && td == 2) || tr == 1 && td == 3)
+                        continue
+                    var el = list[0].children[tr].children[td].children[i].lastChild;
+                    if (el != null)
+                        text += el.textContent + " , ";
+                }
+            }
+        }
+        if (text != '')
+            text = text.substring(0, text.length - 2);
+        $('#btnA').text("(AUB)' = { " + text + " }");
+
+    } else if (action == 'A^B_N') {
+        //(A^B)'
+        $('.example-table').css('background-image', "url(../Images/SetA^B_N.png)");
+        text = "";
+        var list = document.getElementById('tblMain').children;
+        for (let tr = 0; tr < 3; tr++) {
+            for (let td = 0; td < 5; td++) {
+                for (var i = 0; i < list[0].children[tr].children[td].children.length; i++) {
+                    if ((tr == 1 && td == 2))
+                        continue
+                    var el = list[0].children[tr].children[td].children[i].lastChild;
+                    if (el != null)
+                        text += el.textContent + " , ";
+                }
+            }
+        }
+        if (text != '')
+            text = text.substring(0, text.length - 2);
+        $('#btnA').text("(A^B)' = { " + text + " }");
+
+    }
+}
+
+function paly(){
+    var play = $('#btnPlay').text();
+    if(play == "Play"){
+        audio.play();
+      //  $('#btnPlay').text("Pause");
+        setTimeout(function(){  
+           
+        $('#imgImage2').css('visibility', 'visible');
+        $('#imgImage2').css('background-image', 'url(../Images/SetA.png)');
+        
+        $('#btnPlay').css('visibility', 'hidden');
+
+        }, 23000);
+       
+    }
+    else{
+        audio.pause();
+        $('#btnPlay').text("Play");
+    }
+}
+
+function VisibleFalseButtons(){
+   $('#dragtarget1').css('visibility', 'hidden');
+   $('#dragtarget2').css('visibility', 'hidden');
+   $('#dragtarget3').css('visibility', 'hidden');
+   $('#dragtarget4').css('visibility', 'hidden');
+   $('#dragtarget5').css('visibility', 'hidden');
+   $('#dragtarget6').css('visibility', 'hidden');
+   $('#dragtarget7').css('visibility', 'hidden');
+   $('#dragtarget8').css('visibility', 'hidden');
+}
+
+function VisibleTrueButtons(){
+    $('#dragtarget1').css('visibility', 'visible');
+    $('#dragtarget2').css('visibility', 'visible');
+    $('#dragtarget3').css('visibility', 'visible');
+    $('#dragtarget4').css('visibility', 'visible');
+    $('#dragtarget5').css('visibility', 'visible');
+    $('#dragtarget6').css('visibility', 'visible');
+    $('#dragtarget7').css('visibility', 'visible');
+    $('#dragtarget8').css('visibility', 'visible');
+ }
